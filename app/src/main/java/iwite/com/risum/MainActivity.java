b33605d3e8f2b07ea3259d8ae689ddc077e12952
ruedagato <ruedagato@gmail.com>
2016-08-04 00:23:22 -0500

    Initial commit

---
+++ app/src/main/java/iwite/com/risum/MainActivity.java
@@ -0,0 +1,13 @@
+package iwite.com.risum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
